b8fd8b8f7c5a4273cea9fed320b913eaf7afce6b
Joyce Lau <jlau@pandora.com>
2017-05-16 09:38:21 -0700

    added links

--- src/content/Home.js
+++ src/content/Home.js
@@ -7,30 +7,30 @@ function Home() {
 		<div>
 			<main>
 				<section className="section-default">
-					<p className="font-emphasis"><a href="#">Joyce Lau</a> is a product designer, currently designing visual systems and wrangling sass at <a href="#">Pandora</a> in <a href="#">Oakland, California</a>.</p>
-					<p className="font-primary">I draw things on <a href="#">Instagram</a>, but I'm also professional, so check out <a href="#">Linkedin</a>. <a href="#">Drop a line</a> to see more examples of my work or to just chat.</p>
+					<p className="font-emphasis"><a href="#">Joyce Lau</a> is a product designer, currently designing visual systems and wrangling sass at <a target="blank" href="https://pandora.com">Pandora</a> in <a target="blank" href="#">Oakland, California</a>.</p>
+					<p className="font-primary">I draw things on <a href="#">Instagram</a>, but I'm also professional, so check out <a href="#">Linkedin</a>. <a href="mailto:ohjoycelau@gmail.com?subject=Ahoy!">Drop a line</a> to see more examples of my work or to just chat.</p>
 				</section>
 				<section className="section-default">
 					<p className="font-primary">Lately I've been up to,</p>
 					<p>
 						<h2 className="font-primary">Pandora</h2>
-						<h1 className="font-emphasis">Designing Pandora's user experience on <a href="#">Roku</a> and <a href="#">Android TV</a>.</h1>
+						<h1 className="font-emphasis">Designing Pandora's user experience on <a target="blank" href="https://channelstore.roku.com/details/28/pandora">Roku</a> and <a target="blank" href="https://play.google.com/store/apps/details?id=com.pandora.android.atv&hl=en">Android TV</a>.</h1>
 					</p>
 					<p>
 						<h2 className="font-primary">Uncharted Minds</h2>
-						<h1 className="font-emphasis">Once, I spoke on a panel about <a href="#">creativity</a>.</h1>
+						<h1 className="font-emphasis">I spoke on a panel about <a target="blank" href="https://www.eventbrite.com/e/design-gurus-summit-lyft-airbnb-microsoft-pandora-cooper-and-more-tickets-22736032084#">creativity</a>.</h1>
 					</p>
 					<p>
 						<h2 className="font-primary">Pandora</h2>
-						<h1 className="font-emphasis">Designing and developing Pandora <a href="#">Careers</a> and <a href="#">Blog</a>, among <a href="#">others</a>.</h1>
+						<h1 className="font-emphasis">Designing and developing Pandora <a target="blank" href="http://www.pandora.com/careers/">Careers</a>, <a target="blank" href="http://blog.pandora.com/">Blog</a> and internal tools.</h1>
 					</p>
 					<p>
 						<h2 className="font-primary">US-China Exchange</h2>
-						<h1 className="font-emphasis">Translating designs into <a href="#">responsive web</a> at USC Annenberg Public Affairs.</h1>
+						<h1 className="font-emphasis">Translating designs into <a target="blank" href="http://uschinaexchange.usc.edu/content/uscpeking-university-report-building-us-china-trust-be-released-april-22-2014">responsive web</a> at USC Annenberg Public Affairs.</h1>
 					</p>
 				</section>
 				<section className="section-default section-last">
-					<button className="space-right-s" href="">Resume</button><button href="">Drop a line</button>
+					<a className="button space-right-s" href="">Resume</a><a className="button" href="mailto:ohjoycelau@gmail.com?subject=Ahoy!">Drop a line</a>
 				</section>
 			</main>
 		</div>
