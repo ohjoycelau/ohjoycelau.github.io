aaa37524e1a67808123fc91784971dda235b2604
Joyce Lau <jlau@pandora.com>
2017-05-27 16:06:20 -0700

    wording updates

--- src/content/Home.js
+++ src/content/Home.js
@@ -8,13 +8,13 @@ function Home() {
 			<main>
 				<section className="section-default">
 					<p className="font-emphasis"><Link to="/Resume">Joyce Lau</Link> is a product designer, currently designing visual systems and wrangling sass at <a target="blank" href="https://pandora.com">Pandora</a> in <a target="blank" href="https://www.google.com/maps/placelists/list/1Txuf3VoxvgevYPY3ZfRVgmjlqcU">Oakland,&nbsp;California</a>.</p>
-					<p className="font-primary">I draw things on <a href="http://instagram.com/ohjoycelau" target="blank">Instagram</a>, but I'm also professional, so check out <a href="http://linkedin.com/in/ohjoycelau" target="blank">Linkedin</a>. <a href="mailto:ohjoycelau@gmail.com?subject=Ahoy!" target="blank">Drop a line</a> to see more examples of my work or to just chat.</p>
+					<p className="font-primary">I draw things on <a href="http://instagram.com/ohjoycelau" target="blank">Instagram</a>, but I'm also professional, so check out <a href="http://linkedin.com/in/ohjoycelau" target="blank">Linkedin</a>. <a href="mailto:ohjoycelau@gmail.com?subject=Ahoy!" target="blank">Drop&nbsp;a&nbsp;line</a> for inquiries or just to chat.</p>
 				</section>
 				<section className="section-default">
 					<p className="font-primary">Lately I've been up to,</p>
 					<p>
 						<h2 className="font-primary">Pandora</h2>
-						<h1 className="font-emphasis">Designing Pandora's user experience on <a target="blank" href="https://channelstore.roku.com/details/28/pandora">Roku</a> and <a target="blank" href="https://play.google.com/store/apps/details?id=com.pandora.android.atv&hl=en">Android TV</a>.</h1>
+						<h1 className="font-emphasis">Designing Pandora's user experience on <a target="blank" href="https://channelstore.roku.com/details/28/pandora">Roku</a> and <a target="blank" href="https://play.google.com/store/apps/details?id=com.pandora.android.atv&hl=en">Android&nbsp;TV</a>.</h1>
 					</p>
 					<p>
 						<h2 className="font-primary">Uncharted Minds</h2>
@@ -26,7 +26,7 @@ function Home() {
 					</p>
 					<p>
 						<h2 className="font-primary">US-China Exchange</h2>
-						<h1 className="font-emphasis">Translating designs into <a target="blank" href="http://uschinaexchange.usc.edu/content/uscpeking-university-report-building-us-china-trust-be-released-april-22-2014">responsive web</a> at USC Annenberg Public Affairs.</h1>
+						<h1 className="font-emphasis">Translating designs into <a target="blank" href="http://uschinaexchange.usc.edu/content/uscpeking-university-report-building-us-china-trust-be-released-april-22-2014">responsive&nbsp;web</a> at USC Annenberg Public Affairs.</h1>
 					</p>
 				</section>
 				<section className="section-full">
