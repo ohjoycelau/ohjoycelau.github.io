fba04472fc9fb0097169f9ba4c6f33b31bf95da1
Joyce Lau <joycelau@Eunices-MacBook-Pro.local>
2018-08-12 16:53:17 -0700

    merged 180812_updates to pre-build

---
+++ deploy-ftp.js
@@ -0,0 +1,26 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+    user: "joycelau",                   // NOTE that this was username in 1.x 
+    // password: "password",            // optional, prompted if none given
+    host: "joycelau.org",
+    port: 21,
+    localRoot: __dirname + '/build',
+    remoteRoot: '/public_html',
+    include: ['*', '**/*'],             // this would upload everything except dot files
+    // include: ['*.php', 'dist/*'],
+    exclude: ['dist/**/*.map'],         // e.g. exclude sourcemaps
+    deleteRemote: false                 // delete existing files at destination before uploading
+}
+
+// use with promises
+ftpDeploy.deploy(config)
+    .then(res => console.log('finished'))
+    .catch(err => console.log(err))
+
+// use with callback
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
+});
