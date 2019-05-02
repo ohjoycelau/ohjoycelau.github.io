eb75f57e567280b1162c055bcd46423b356bcb8c
Joyce Lau <ohjoycelau@github.com>
2019-05-01 22:01:40 -0700

    initial from codepen

--- src/content/Home.js
+++ src/content/Home.js
@@ -6,15 +6,14 @@ function Home() {
 	return (
 		<div>
 			<main>
-				<section className="section-first">
-					<p className="font-h1"><Link to="/Resume">joyce lau</Link> is an interaction designer based in beautiful <a target="blank" href="https://www.google.com/maps/placelists/list/1Txuf3VoxvgevYPY3ZfRVgmjlqcU">oakland,&nbsp;california</a>.</p>
-				</section>
-				<section className="section-default">
-					<p className="font-h2">currently writing narratives at <a href="http://github.com" target="blank">github</a>, previously herding visual systems and wrangling sass at <a target="blank" href="https://pandora.com">pandora</a>.</p>
-				</section>
-				<section className="section-default">
-					<Link className="button button-right" to="/Resume">résumé</Link><br/>
-					<Link className="button button-right" to="/Pandora-10ft">see projects</Link>
+				<section class="section-default py-8">
+					<h1 class="pb-4"><a href="#">joyce lau</a> is an interaction designer based in beautiful <a href="#">oakland, california</a></h1>
+					<h2 class="pb-4">currently writing narratives at <a href="#">github</a> previously herding visual systems and wrangling sass at <a href="#">pandora</a>.</h2>
+					<ul class="font-h2">
+						<li><a href="#">pandora re-design on tv</a></li>
+						<li><a href="#">the seamless home experience</a></li>
+						<li><a href="#">design systems at pandora</a></li>
+					</ul>
 				</section>
 			</main>
 		</div>
